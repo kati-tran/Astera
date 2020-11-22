7d01dbe5bc256a779488abd3a2c1418f77fd42ad
Shawn Agustin <sragusti@uci.edu>
2020-11-21 19:01:12 -0800

    fixed logic

--- astera/Assets/Scripts/triggerPoint.cs
+++ astera/Assets/Scripts/triggerPoint.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class triggerPoint : MonoBehaviour
 {
     public GameObject bird;
+    public GameObject player;
+    PlayerMove playerScript;
+
+    void Start(){
+        playerScript = player.GetComponent<PlayerMove>();
+    }
+
     private void OnTriggerEnter2D(Collider2D col){
         bird.SetActive(true);
+        playerScript.activeSpirit = PlayerMove.Spirit.Bird;
         Object.Destroy(this.gameObject);
     }
 }
