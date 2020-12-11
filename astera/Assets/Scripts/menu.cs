7752bdc3580205914fe6956523a7c5789d5cdcd3
sandra-soto <sandra909@live.com>
2020-12-11 06:45:37 -0800

    new UI + how to play menu

---
+++ astera/Assets/Scripts/menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+	public GameObject ToggledPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TogglePanel()
+    {
+  
+    	ToggledPanel.gameObject.SetActive(!ToggledPanel.gameObject.activeSelf);
+    }
+}
