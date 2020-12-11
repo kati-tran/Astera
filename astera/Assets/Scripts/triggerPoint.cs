05536e302afef4eb510fe2cee7c6fdf664f913a4
Kati T <45055772+jigglequack@users.noreply.github.com>
2020-12-10 19:50:42 -0800

    bird scene sound effect edits

--- astera/Assets/Scripts/triggerPoint.cs
+++ astera/Assets/Scripts/triggerPoint.cs
@@ -7,13 +7,17 @@ public class triggerPoint : MonoBehaviour
     public GameObject bird;
     public GameObject player;
     PlayerMove playerScript;
+    public GameObject birdsound;
+    AudioSource birdS;
 
     void Start(){
         playerScript = player.GetComponent<PlayerMove>();
+        birdS = birdsound.GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D col){
         bird.SetActive(true);
+        birdS.Play(0);
         playerScript.activeSpirit = PlayerMove.Spirit.Bird;
         Object.Destroy(this.gameObject);
     }
