7f7952a15d4eadeac4276d1c8618fad4b021a4fe
Kasen Chaque <kchaque@uci.edu>
2020-12-13 18:58:03 -0800

    Fox lead

--- astera/Assets/Scripts/FoxScript.cs
+++ astera/Assets/Scripts/FoxScript.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class FoxScript : MonoBehaviour
 {
+    bool leading = false;
     bool active = false;
     bool initialAnimation = false;
     bool initAnimTargetReached = false;
     Vector3 initAnimTargetPos;
     float initAnimWaitStartTime;
     bool facingLeft = true;
+    Vector3 initialPos;
 
     public GameObject player;
     public float followDistance = 1;
@@ -17,8 +20,24 @@ public class FoxScript : MonoBehaviour
     public float followSpeed = 1;
     public float initAnimWalkSpeed = 1;
     public float initAnimWaitTime = 1;
-
+    public float leadMoveSpeed = 15;
+    public float leadStartDist = 30;
+    public float leadApproachDist = 10;
+    public List<TargetPos> targetPos;
     Animator anim;
+
+    [System.Serializable]
+    public struct TargetPos
+    {
+        public TargetPos(Vector3 position, bool waitForPlayer)
+        {
+            pos = position;
+            wait = waitForPlayer;
+        }
+
+        public Vector3 pos;
+        public bool wait;
+    }
 
     void Start()
     {
@@ -26,7 +45,7 @@ public class FoxScript : MonoBehaviour
             throw new System.NullReferenceException("Player GameObject reference not set or invalid on Fox Script");
         if (followDistance <= 0 || followSpeed <= 0)
             throw new System.Exception("FoxScript fields followDistance and followSpeed must be positive and nonzero");
-
+        initialPos = transform.position;
         anim = GetComponent<Animator>();
     }
 
@@ -39,6 +58,60 @@ public class FoxScript : MonoBehaviour
             initAnimTargetPos = transform.position + new Vector3(-7, 0);
             FindObjectOfType<AudioManager>().Play("FoxCollect");
         }
+    }
+
+    public void Lead()
+    {
+        if(!active && !leading)
+        {
+            leading = true;
+            List<TargetPos> t = new List<TargetPos>
+            {
+                new TargetPos(new Vector3(player.transform.position.x - leadStartDist, targetPos[0].pos.y, targetPos[0].pos.z), true)
+            };
+            t.AddRange(targetPos);
+            StartCoroutine(_lead(t));
+        }
+    }
+
+    private IEnumerator _lead(List<TargetPos> targets)
+    {
+        transform.position = targetPos[0].pos;
+        foreach(TargetPos t in targets)
+        {
+            if (t.pos.x < transform.position.x)
+                transform.rotation = Quaternion.Euler(0, -90, 0);
+            else if (t.pos.x > transform.position.x)
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+            while(transform.position != t.pos)
+            {
+                Vector3 dist = t.pos - transform.position;
+                Vector2 dist2 = dist;
+                float moveAmount = Time.deltaTime * leadMoveSpeed;
+                if (dist2.magnitude <= moveAmount)
+                {
+                    transform.position = t.pos;
+                    anim.SetFloat("speed", 0);
+                }
+                else
+                {
+                    Vector3 movement = dist2.normalized * moveAmount;
+                    movement.z = dist.z * moveAmount / dist2.magnitude;
+                    transform.position += movement;
+                    anim.SetFloat("speed", leadMoveSpeed);
+                }
+                yield return null;
+            }
+            if(t.wait)
+            {
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                while (((Vector2)(player.transform.position - transform.position)).magnitude > leadApproachDist)
+                    yield return null;
+            }
+        }
+
+        transform.position = initialPos;
+        leading = false;
     }
 
     void Update()
---
+++ astera/Assets/Scripts/leadTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leadTrigger : MonoBehaviour
+{
+    FoxScript fox;
+
+    void Start()
+    {
+        fox = FindObjectOfType<FoxScript>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerMove move = other.gameObject.GetComponent<PlayerMove>();
+        if(move != null && other == move.groundContactCollider)
+        {
+            fox.Lead();
+            Destroy(gameObject);
+        }
+    }
+}
